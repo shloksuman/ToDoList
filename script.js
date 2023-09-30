bb74ff3785533d71b157095f847e18d03a9f7e95
Shlok Kumar Suman <shloksuman01@gmail.com>
2023-09-30 10:15:06 +0530

    completed good to go

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value ;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7" ; /* add x (cross) icon */
+        li.appendChild(span); /* display the span after li in same line */
+    }
+    inputBox.value = '';
+    saveData();
+}
+
+listContainer.addEventListener("click" , function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false) ;
+
+//setting up local storage
+
+function saveData(){
+    localStorage.setItem("data" , listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
